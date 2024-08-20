5af4277e07a8909ed7f2fe772454c240ed7d0043
Charles Talbot <charles.talbot@bjss.com>
2024-08-20 11:16:35 +0100

    added tests and reformatted production code

--- main.go
+++ main.go
@@ -10,19 +10,13 @@ type Ticket struct {
 	TicketKey   string
 	Summary     string
 	Description string
-	Status      string
-	Priority    string
-	Assignee    string
-	Reporter    string
-	Created     string
-	Updated     string
 }
 
-func getTicketInformation() (string, string, error) {
+func getTicketInformation() (string, string, string, error) {
 	//opening csv file
 	file, err := os.Open("ticket.csv")
 	if err != nil {
-		return "", "", errors.New("error when opening ticket page") //returns no value for the strings and returns specific error for opening page error
+		return "", "", "", errors.New("error when opening ticket page") //returns no value for the strings and returns specific error for opening page error
 	}
 	defer file.Close()
 
@@ -30,7 +24,7 @@ func getTicketInformation() (string, string, error) {
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
-		return "", "", errors.New("error reading csv file") //returns specific error for not reading csv file
+		return "", "", "", errors.New("error reading csv file") //returns specific error for not reading csv file
 	}
 
 	for _, record := range records[1:] {
@@ -38,14 +32,8 @@ func getTicketInformation() (string, string, error) {
 			TicketKey:   record[0],
 			Summary:     record[1],
 			Description: record[2],
-			Status:      record[3],
-			Priority:    record[4],
-			Assignee:    record[5],
-			Reporter:    record[6],
-			Created:     record[7],
-			Updated:     record[8],
 		}
-		return ticket.Summary, ticket.Description, nil
+		return ticket.TicketKey, ticket.Summary, ticket.Description, nil
 	}
-	return "all tickets have been processed", "", nil
+	return "all tickets have been processed", "", "", nil
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestGetTicketSummaryInformation(t *testing.T) {
 	//arrange
-	got, _, _ := getTicketInformation()
+	_, got, _, err := getTicketInformation()
+	fmt.Println(err)
 	want := "Implement User Authentication"
 
 	if got != want {
@@ -15,12 +17,26 @@ func TestGetTicketSummaryInformation(t *testing.T) {
 
 }
 
-/*
-func testGetTicketDescriptionInformation(t *testing.T) {
+func TestGetTicketKeyInformation(t *testing.T) {
 	//arrange
-	ticketDescription := getTicketInformation()
+	got, _, _, _ := getTicketInformation()
 
-	descriptionWant := "As a security team member I would like to implement a user suthentication to test application to enhance security"
+	want := "ATT-1588"
+
+	if got != want {
+		t.Errorf("got %v, expected %v", got, want)
+	}
 
 }
-*/
+
+func TestGetTicketDescriptionInformation(t *testing.T) {
+	//arrange
+	_, _, got, _ := getTicketInformation()
+
+	want := "As a security team member I would like to implement a user suthentication to test application to enhance security"
+
+	if got != want {
+		t.Errorf("got %v, expected %v", got, want)
+	}
+
+}
