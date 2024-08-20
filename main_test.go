f663b7715d201fdf496c9029ab42f4f22f9fa928
Charles Talbot <charles.talbot@bjss.com>
2024-08-20 10:49:28 +0100

    added test for getting summary info

--- main_test.go
+++ main_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestGetTicketSummaryInformation(t *testing.T) {
+	//arrange
+	got, _, _ := getTicketInformation()
+	want := "Implement User Authentication"
+
+	if got != want {
+		t.Errorf("got %v, expected %v", got, want)
+	}
+
+}
+
+/*
+func testGetTicketDescriptionInformation(t *testing.T) {
+	//arrange
+	ticketDescription := getTicketInformation()
+
+	descriptionWant := "As a security team member I would like to implement a user suthentication to test application to enhance security"
+
+}
+*/
